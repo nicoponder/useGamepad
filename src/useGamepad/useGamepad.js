f6d9ab27fea9c96399cc9f59464692b5d357c426
Nico Ponder <nicoponder@gmail.com>
2020-07-04 14:09:38 -0600

    Add draft of gamepad+connect hooks
    Adds hooks which can be used on an SFC to receive gamepad state.
    Still need to split useGamepad into separate hooks for handlers/state
    for perf reasons, and need to address perf workaround in the
    connection hook

--- src/useGamepad/useGamepad.js
+++ src/useGamepad/useGamepad.js
@@ -1,2 +1,59 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import {forEach, get} from 'lodash';
+
 import useGamepadConnection from "./useGamepadConnection";
+
+export default function useGamepad({mappings, onConnect, onDisconnect}) {
+  const [gamepads, _setGamepads] = useState([]);
+  const frame = useRef(0);
+
+  useGamepadConnection({
+    onConnect: ({gamepad}) => onConnect(gamepad),
+    onDisconnect: ({gamepad}) => onDisconnect(gamepad),
+  })
+
+  function updateStatus() {
+    frame.current++;
+    const curGamepads = navigator.getGamepads();
+    _setGamepads(curGamepads);
+
+
+    forEach(curGamepads, (gamepad) => {
+      const mappingId = gamepad && gamepad.id.split('(')[0].trim();
+      if (get(mappings, `[${mappingId}]`)) {
+        if (gamepad.axes) {
+          gamepad.axes.forEach((axis, index) => {
+            const {handler} = get(mappings, `[${mappingId}].axes[${index}]`);
+            if (axis) {
+              handler(axis)
+            }
+          });
+        }
+        if (gamepad.buttons) {
+          gamepad.buttons.forEach((button, index) => {
+            const {handler} = get(mappings, `[${mappingId}].buttons[${index}]`);
+            if (button.value) {
+              handler(button.value)
+            }
+          });
+        }
+      }
+    });
+
+    return window.requestAnimationFrame(updateStatus);
+  }
+
+    // console.log('frame #', frame.current, navigator.getGamepads()[0], gamepads);
+
+  useEffect(() => {
+    const requestId = requestAnimationFrame(updateStatus);
+
+    return () => {
+      window.clearAnimationFrame && window.clearAnimationFrame(requestId);
+    }
+  }, []);
+
+  return {
+    gamepads
+  };
+}
--- src/useGamepad/useGamepadConnection.js
+++ src/useGamepad/useGamepadConnection.js
@@ -18,17 +18,24 @@ function _onDisconnect(e) {
   );
 }
 
-export default function useGamepadConnection(
+export default function useGamepadConnection({
   onConnect = _onConnect,
   onDisconnect = _onDisconnect
-) {
+}) {
   useEffect(() => {
     window.addEventListener("gamepadconnected", onConnect);
     window.addEventListener("gamepaddisconnected", onDisconnect);
 
     return () => {
-      window.removeEventListener("gamepadconnected");
-      window.removeEventListener("gamepadconnected");
+      window.removeEventListener("gamepadconnected", onConnect);
+      window.removeEventListener("gamepaddisconnected", onDisconnect);
     };
-  }, [onConnect, onDisconnect]);
+    // TODO: adding onConnect/onDisconnect as effect deps causes severe perf
+    // problems and results in dropped updates. Need to investigate why this is,
+    // bc I thought lifting definitions to outside of the hook would allow this.
+    // Possibly because:
+    // * they're overridden in-props
+    // * parent is supplying fresh callbacks
+    // * some other cause
+  }, []);
 }
